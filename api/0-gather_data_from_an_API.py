8719281e8701fb3b18328cdbc574fa4e41a72fc3
RamziHkiri <6257@holbertonstudents.com>
2023-09-05 15:36:42 +0200

    fix pep8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,23 +5,24 @@ import sys
 import requests
 import json
 
-if __name__=="__main__":
+if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com"
-    emp_id =sys.argv[1]
+    emp_id = sys.argv[1]
 
-    res = requests.get("{}/users/{}/todos".format(api_url, emp_id),params={"_expand": "user"})
+    res = requests.get("{}/users/{}/todos".format(api_url, emp_id),
+                       params={"_expand": "user"})
     data = res.json()
     employee_name = data[0]["user"]["name"]
     done_tasks_number = 0
-    done_tasks_list=[]
+    done_tasks_list = []
     for task in data:
         if task["completed"]:
-            done_tasks_number +=1
+            done_tasks_number += 1
             done_tasks_list.append(task)
-    total_tasks=len(data)
-   
-    
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks_number,total_tasks))  
+    total_tasks = len(data)
+
+    print("Employee {} is done with tasks({}/{}):"
+        .format(employee_name, done_tasks_number, total_tasks))  
     for task in done_tasks_list:
-        print(f"\t {task['title']}")
+        print("\t {}".format(task["title"]))
     
