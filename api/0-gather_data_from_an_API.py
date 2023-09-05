edd25e52a9202b503fc3f22f698c2c45d08f181d
RamziHkiri <6257@holbertonstudents.com>
2023-09-05 15:28:58 +0200

    get data from restapi

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a Python script that, using REST API"""
+
+import sys
+import requests
+import json
+
+if __name__=="__main__":
+    api_url = "https://jsonplaceholder.typicode.com"
+    emp_id =sys.argv[1]
+
+    res = requests.get("{}/users/{}/todos".format(api_url, emp_id),params={"_expand": "user"})
+    data = res.json()
+    employee_name = data[0]["user"]["name"]
+    done_tasks_number = 0
+    done_tasks_list=[]
+    for task in data:
+        if task["completed"]:
+            done_tasks_number +=1
+            done_tasks_list.append(task)
+    total_tasks=len(data)
+   
+    
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks_number,total_tasks))  
+    for task in done_tasks_list:
+        print(f"\t {task['title']}")
+    
