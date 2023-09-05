e5f070e638dcee27a2705e1609f64e429cad57d0
RamziHkiri <6257@holbertonstudents.com>
2023-09-05 16:22:30 +0200

    export data from rest api to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a Python script that, using REST API"""
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com"
+    emp_id = sys.argv[1]
+
+    res = requests.get("{}/users/{}/todos".format(api_url, emp_id),
+                       params={"_expand": "user"})
+    data = res.json()
+    employee_name = data[0]["user"]["name"]
+with open('{}.csv'.format(emp_id), 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    for task in data:
+        spamwriter.writerow([emp_id, employee_name,
+                            task["completed"], task["title"]])
