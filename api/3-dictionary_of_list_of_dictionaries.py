37d48c9e17a47b259548f5be57fd43462087fd20
RamziHkiri <6257@holbertonstudents.com>
2023-09-05 19:33:42 +0200

    3-dictionary_of_list_of_dictionaries.py

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a Python script that, using REST API
+and export data in the JSON format. """
+import json
+import requests
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    res1 = requests.get("{}/users".format(api_url))
+    data1 = res1.json()
+    emp_dic = {}
+
+    for user in data1:
+        res2 = requests.get(f"{api_url}/users/{user['id']}/todos")
+        data2 = res2.json() 
+        emp_dic[user["id"]] = []
+        for task in data2:
+            dic_task = {"task": task["title"], "completed": task["completed"],
+                        "username": user["username"]}
+            emp_dic[user["id"]].append(dic_task)
+
+    with open("todo_all_employees.json", "w")as jsfile:
+        json.dump(emp_dic, jsfile)
